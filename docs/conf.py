77fcc938da373999d4faa45b9bb724f86628c37f
Piotr Roszatycki <piotr.roszatycki@teradata.com>
2016-03-22 11:57:15 +0100

    Use mock for Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,16 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['pyev']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
